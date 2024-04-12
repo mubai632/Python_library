ceaea0c92bcbaab9f16e26a6a7b70a8e1ceccbba
mubai632 <1316261536@qq.com>
2024-04-12 23:06:05 +0800

    Changes

--- User_login.py
+++ User_login.py
@@ -8,5 +8,5 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 class User_login_windows(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi('./ui/user_界面.ui')
+        self.ui = uic.loadUi('./ui_user/user_界面.ui')
 
